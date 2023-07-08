8c1f0dd6da02ce783141a3105ddb311e8d8bc87d
hendra-saputraa <muhhendrasaputra93@gmail.com>
2023-07-08 00:59:11 +0000

    sandang-admin

--- app/(dashboard)/[storeId]/(routes)/kategori/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/kategori/components/cell-action.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import axios from "axios";
-import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
-import { useParams, useRouter } from "next/navigation";
 import React, { useState } from "react";
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
 import { toast } from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
 
-import { AlertModal } from "@/components/modals/alert-modal";
 import { Button } from "@/components/ui/button";
 import { 
   DropdownMenu, 
@@ -15,38 +14,40 @@ import {
   DropdownMenuLabel, 
   DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
+import { useCategoryModal } from "@/hooks/use-category-modal";
+import { AlertModal } from "@/components/modals/alert-modal";
 
-import { ProductColumn } from "./columns";
+import { CategoryColumn } from "./columns";
 
 interface CellActionProps {
-  data: ProductColumn;
+  data: CategoryColumn;
 }
 
 export const CellAction: React.FC<CellActionProps> = ({
   data,
 }) => {
-  const [loading, setLoading] = useState(false);
-  const [open, setOpen] = useState(false);
   const router = useRouter();
   const params = useParams();
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const onConfirm = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/${params.storeId}/kategori/${data.id}`);
-      toast.success('Kategori dihapus.');
+      await axios.delete(`/api/${params.storeId}/categories/${data.id}`);
+      toast.success('Category deleted.');
       router.refresh();
     } catch (error) {
-      toast.error('Ada yang salah, mohon coba kembali.');
+      toast.error('Make sure you removed all products using this category first.');
     } finally {
-      setLoading(false);
       setOpen(false);
+      setLoading(false);
     }
   };
 
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id);
-    toast.success('Id kategori berhasil disalin.');
+    toast.success('Category ID copied to clipboard.');
   }
 
   return (
@@ -60,26 +61,26 @@ export const CellAction: React.FC<CellActionProps> = ({
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="h-8 w-8 p-0">
-            <span className="sr-only">Buka Menu</span>
+            <span className="sr-only">Open menu</span>
             <MoreHorizontal className="h-4 w-4" />
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
-          <DropdownMenuLabel>Aksi</DropdownMenuLabel>
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
           <DropdownMenuItem
             onClick={() => onCopy(data.id)}
           >
-            <Copy className="mr-2 h-4 w-4" /> Salin Id
+            <Copy className="mr-2 h-4 w-4" /> Copy Id
           </DropdownMenuItem>
           <DropdownMenuItem
-            onClick={() => router.push(`/${params.storeId}/kategori/${data.id}`)}
+            onClick={() => router.push(`/${params.storeId}/categories/${data.id}`)}
           >
-            <Edit className="mr-2 h-4 w-4" /> Edit
+            <Edit className="mr-2 h-4 w-4" /> Update
           </DropdownMenuItem>
           <DropdownMenuItem
             onClick={() => setOpen(true)}
           >
-            <Trash className="mr-2 h-4 w-4" /> Hapus
+            <Trash className="mr-2 h-4 w-4" /> Delete
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
